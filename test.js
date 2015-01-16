5b8678ac43fcc20888e23d60825de32558b626b3
Karissa McKelvey <krmckelv@gmail.com>
2015-01-16 15:26:02 -0800

    Add test for offset

--- test.js
+++ test.js
@@ -23,7 +23,7 @@ function setupDatabase(cb) {
   })
 }
 
-function searchValues(db, opts, cb) {
+function getValues(db, opts, cb) {
   var values = []
 
   var searchStream = db.createSearchStream(opts)
@@ -46,7 +46,7 @@ test('test that search returns the right rows', function (t) {
       query: 'Build Status'
     }
 
-    searchValues(db, opts, function (err, values) {
+    getValues(db, opts, function (err, values) {
       t.ifError(err)
       t.equals(values.length, 7)
     })
@@ -64,7 +64,7 @@ test('test since', function (t) {
       since: 'AQ'
     }
 
-    searchValues(db, opts, function (err, values) {
+    getValues(db, opts, function (err, values) {
       t.ifError(err)
       t.equals(values.length, 6)
     })
@@ -82,7 +82,7 @@ test('test limit', function (t) {
       limit: 1
     }
 
-    searchValues(db, opts, function (err, values) {
+    getValues(db, opts, function (err, values) {
       t.ifError(err)
       t.equals(values.length, 1)
     })
@@ -101,11 +101,45 @@ test('test since and limit', function (t) {
       since: 'AQ'
     }
 
-    searchValues(db, opts, function (err, values) {
+    getValues(db, opts, function (err, values) {
       t.ifError(err)
       t.equals(values.length, 1)
       var row = values[0]
       t.equals(row.name, 'Backbone.Chosen')
     })
+  })
+})
+
+
+test('test offset', function (t) {
+  setupDatabase(function (err, db)  {
+    t.plan(6)
+
+    var opts = {
+      field: 'readme',
+      query: 'Build Status',
+      limit: 1,
+      offset: 0
+    }
+
+    getValues(db, opts, function (err, values) {
+      t.ifError(err)
+      t.equals(values.length, 1)
+
+      var opts = {
+        field: 'readme',
+        query: 'Build Status',
+        limit: 1,
+        offset: 1
+      }
+
+      getValues(db, opts, function (err, values2) {
+        t.ifError(err)
+        t.equals(values2.length, 1)
+        t.notEquals(values2[0], values[0], 'with offset, should get a different value')
+        t.true(values2[0].name > values[0].name, 'with offset, the new value should be greater')
+      })
+    })
+
   })
 })
